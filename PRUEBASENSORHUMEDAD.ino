f4253b23a63af671d06a29caa4804c01a862ea4c
maxarg <81944422+maxarg@users.noreply.github.com>
2023-07-14 14:34:40 -0300

    Add files via upload

---
+++ PRUEBASENSORHUMEDAD.ino
@@ -0,0 +1,18 @@
+
+// Definir el número de pin para la entrada analógica
+const int pinAnalogico = 32;
+
+void setup() {
+  // Inicializar el puerto serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Leer el valor analógico del pin GPIO32
+  int valorAnalogico = analogRead(pinAnalogico);
+
+  // Imprimir el valor leído en el monitor serie
+  Serial.print("Valor analógico: ");
+  Serial.println(valorAnalogico);
+
+}
